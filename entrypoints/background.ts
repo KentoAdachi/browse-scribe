75575ee22d34dd7d873069867896bd5ea735e42f
KentoAdachi <k.adachi0312@gmail.com>
2025-05-04 14:39:32 +0900

    feat: add command handling for opening popup and side panel

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,6 +1,15 @@
 export default defineBackground(() => {
   console.log("WebNote background script is running");
 
-  // Set up side panel to open when clicking the action button
   browser.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
+
+  browser.commands.onCommand.addListener(async (command, tab) => {
+    console.log(`Command received: ${command}`);
+
+    if (command === "open_popup") {
+      browser.action.openPopup();
+    } else if (command === "open_side_panel" && tab?.id) {
+      browser.sidePanel.open({ tabId: tab.id });
+    }
+  });
 });
